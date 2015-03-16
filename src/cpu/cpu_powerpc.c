7d3ea125734e2dfb4143cd1e33a0d8ee84bce8fc
Prerna Saxena <prerna@linux.vnet.ibm.com>
2015-03-16 05:49:56 -0500

    PowerPC : Do not allow an empty model spec for 'host-model'
    On PowerPC, a guest VM having CPU mode as 'host-model'
    represents a 'compat' mode VM.
    Forbid a null mode in this case.
    
    Fixes bz 121575 & 122055
    
    Signed-off-by: Prerna Saxena <prerna@linux.vnet.ibm.com>

--- src/cpu/cpu_powerpc.c
+++ src/cpu/cpu_powerpc.c
@@ -549,12 +549,12 @@ ppcUpdate(virCPUDefPtr guest,
           const virCPUDef *host)
 {
     switch ((virCPUMode) guest->mode) {
-    case VIR_CPU_MODE_HOST_MODEL:
     case VIR_CPU_MODE_HOST_PASSTHROUGH:
         guest->match = VIR_CPU_MATCH_EXACT;
         virCPUDefFreeModel(guest);
         return virCPUDefCopyModel(guest, host, true);
 
+    case VIR_CPU_MODE_HOST_MODEL:
     case VIR_CPU_MODE_CUSTOM:
         return 0;
 
