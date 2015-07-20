28d8e01195cb52684506ac3929e9f397c2840169
Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>
2015-07-20 17:48:34 +0530

    Wait for vfio-pci device cleanups before reassinging the device to host driver
    The libvirt today waits for pci-stub driver alone. The same wait is needed for
    vfio devices too.
    
    Signed-off-by: Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>
    LTC-Bugzilla : 127676

--- src/util/virhostdev.c
+++ src/util/virhostdev.c
@@ -796,6 +796,13 @@ virHostdevReattachPCIDevice(virHostdevManagerPtr mgr,
             usleep(100*1000);
             retries--;
         }
+    } else if (STREQ(virPCIDeviceGetStubDriver(dev), "vfio-pci")) {
+        int retries = 100;
+        while (virPCIDeviceWaitForCleanup(dev, "vfio-pci")
+               && retries) {
+            usleep(100*1000);
+            retries--;
+        }
     }
 
     VIR_DEBUG("Reattaching PCI device %s", virPCIDeviceGetName(actual));
