111c3e6daa6538abfae98c4068d48ffff7db553e
Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>
2016-02-22 18:22:26 +0530

    Revert "Split reprobe action from the virPCIUnbindFromStub into a new function"
    This reverts commit c31aa9842244fecfabd45d943924a96a2b5793a8.
    Upstream has a different approach to the unbind. Lets adopt that for now.

--- src/util/virpci.c
+++ src/util/virpci.c
@@ -1090,33 +1090,6 @@ virPCIDeviceUnbind(virPCIDevicePtr dev)
     return ret;
 }
 
-static int virPCIDeviceReprobeHostDriver(virPCIDevicePtr dev, char *driver, char *drvdir)
-{
-    char *path = NULL;
-    int result = -1;
-
-    /* Trigger a re-probe of the device is not in the stub's dynamic
-     * ID table. If the stub is available, but 'remove_id' isn't
-     * available, then re-probing would just cause the device to be
-     * re-bound to the stub.
-     */
-    if (driver && !(path = virPCIDriverFile(driver, "remove_id")))
-        goto cleanup;
-
-    if (!driver || !virFileExists(drvdir) || virFileExists(path)) {
-        if (virFileWriteStr(PCI_SYSFS "drivers_probe", dev->name, 0) < 0) {
-            virReportSystemError(errno,
-                                 _("Failed to trigger a re-probe for PCI device '%s'"),
-                                 dev->name);
-            goto cleanup;
-        }
-    }
-    result = 0;
- cleanup:
-    VIR_FREE(path);
-    return result;
-}
-
 static int
 virPCIDeviceUnbindFromStub(virPCIDevicePtr dev)
 {
@@ -1172,8 +1145,23 @@ virPCIDeviceUnbindFromStub(virPCIDevicePtr dev)
         goto cleanup;
     }
 
-    if (virPCIDeviceReprobeHostDriver(dev, driver, drvdir) < 0)
+    /* Trigger a re-probe of the device is not in the stub's dynamic
+     * ID table. If the stub is available, but 'remove_id' isn't
+     * available, then re-probing would just cause the device to be
+     * re-bound to the stub.
+     */
+    VIR_FREE(path);
+    if (driver && !(path = virPCIDriverFile(driver, "remove_id")))
         goto cleanup;
+
+    if (!driver || !virFileExists(drvdir) || virFileExists(path)) {
+        if (virFileWriteStr(PCI_SYSFS "drivers_probe", dev->name, 0) < 0) {
+            virReportSystemError(errno,
+                                 _("Failed to trigger a re-probe for PCI device '%s'"),
+                                 dev->name);
+            goto cleanup;
+        }
+    }
 
     result = 0;
 
