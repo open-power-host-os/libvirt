a0123e85f86736ac6799e49e7e79a8ba0ee22901
Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>
2016-06-01 09:41:26 +0000

    Revert "util: don't use netlink to save/set mac for macvtap+passthrough+802.1Qbh"
    This reverts commit 46cf0cefa0168a0d929ca87010f59e1cba6c689b.
    The reordering done in commit 29cc45cb is preserved.

--- src/util/virnetdevmacvlan.c
+++ src/util/virnetdevmacvlan.c
@@ -1008,22 +1008,9 @@ virNetDevMacVLanCreateWithVPortProfile(const char *ifnameRequested,
      * This is especially important when using SRIOV capable cards that
      * emulate their switch in firmware.
      */
-
     if (mode == VIR_NETDEV_MACVLAN_MODE_PASSTHRU) {
-        if (virtPortProfile &&
-            virtPortProfile->virtPortType == VIR_NETDEV_VPORT_PROFILE_8021QBH) {
-            /* The Cisco enic driver (the only card that uses
-             * 802.1Qbh) doesn't support IFLA_VFINFO_LIST, which is
-             * required for virNetDevReplaceNetConfig(), so we must
-             * use this function (which uses ioctl(SIOCGIFHWADDR)
-             * instead or virNetDevReplaceNetConfig()
-             */
-            if (virNetDevReplaceMacAddress(linkdev, macaddress, stateDir) < 0)
-                return -1;
-        } else {
-            if (virNetDevReplaceNetConfig(linkdev, -1, macaddress, -1, stateDir) < 0)
-                return -1;
-        }
+        if (virNetDevReplaceNetConfig(linkdev, -1, macaddress, -1, stateDir) < 0)
+            return -1;
     }
 
     if (ifnameRequested) {
@@ -1202,13 +1189,8 @@ int virNetDevMacVLanDeleteWithVPortProfile(const char *ifname,
         virNetDevMacVLanReleaseName(ifname);
     }
 
-    if (mode == VIR_NETDEV_MACVLAN_MODE_PASSTHRU) {
-        if (virtPortProfile &&
-             virtPortProfile->virtPortType == VIR_NETDEV_VPORT_PROFILE_8021QBH)
-            ignore_value(virNetDevRestoreMacAddress(linkdev, stateDir));
-        else
-            ignore_value(virNetDevRestoreNetConfig(linkdev, vf, stateDir));
-    }
+    if (mode == VIR_NETDEV_MACVLAN_MODE_PASSTHRU)
+        ignore_value(virNetDevRestoreNetConfig(linkdev, vf, stateDir));
 
     virNetlinkEventRemoveClient(0, macaddr, NETLINK_ROUTE);
 
