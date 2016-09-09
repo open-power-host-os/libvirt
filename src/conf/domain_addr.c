39ec507531440338aab13570be66df247cbb85d3
Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>
2016-09-09 17:35:24 +0530

    Release address in function granularity than slot
    The commit 6fe678c is partly reverted. The code is moved around and cant revert
    staright.
    
    Signed-off-by: Shivaprasad G Bhat <sbhat@linux.vnet.ibm.com>

--- src/conf/domain_addr.c
+++ src/conf/domain_addr.c
@@ -660,16 +660,6 @@ virDomainPCIAddressEnsureAddr(virDomainPCIAddressSetPtr addrs,
         goto cleanup;
 
     if (virDeviceInfoPCIAddressPresent(dev)) {
-        /* We do not support hotplug multi-function PCI device now, so we should
-         * reserve the whole slot. The function of the PCI device must be 0.
-         */
-        if (dev->addr.pci.function != 0) {
-            virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
-                           _("Only PCI device addresses with function=0"
-                             " are supported"));
-            goto cleanup;
-        }
-
         if (!virDomainPCIAddressValidate(addrs, &dev->addr.pci,
                                          addrStr, flags, true))
             goto cleanup;
@@ -691,7 +681,11 @@ int
 virDomainPCIAddressReleaseAddr(virDomainPCIAddressSetPtr addrs,
                                virPCIDeviceAddressPtr addr)
 {
-    addrs->buses[addr->bus].slot[addr->slot].functions &= ~(1 << addr->function);
+    if ((addr->multi == VIR_TRISTATE_SWITCH_ABSENT) && (addr->function == 0))
+        addrs->buses[addr->bus].slot[addr->slot].functions = 0;
+    else
+        addrs->buses[addr->bus].slot[addr->slot].functions &= ~(1 << addr->function);
+
     return 0;
 }
 
